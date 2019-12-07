9a6862ba156df273d6e90157cb338ec53c1fe6b7
404DeveloperNotFound <44564111+404DeveloperNotFound@users.noreply.github.com>
2019-12-06 21:51:02 -0500

    Build better Demos

--- Create.sh
+++ Create.sh
@@ -1,16 +1,17 @@
 ######################################
 # Lets download our dictionary file  #
 ######################################
-wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt #this downloads our dictionary file
+#wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt #this downloads our dictionary file
 ######################################
 #      Lets make some passwords      #
 ######################################
-echo -n "Password" | md5sum | tr -d " -" >> hashes.txt
-echo -n "HELLO" | md5sum | tr -d " -" >> hashes.txt
-echo -n "MYSECRET" | md5sum | tr -d " -" >> hashes.txt
-echo -n "Test1234" | md5sum | tr -d " -" >> hashes.txt
-echo -n "P455w0rd" | md5sum | tr -d " -" >> hashes.txt
-echo -n "GuessMe" | md5sum | tr -d " -" >> hashes.txt
-echo -n "S3CuReP455Word" | md5sum | tr -d " -" >> hashes.txt
-echo -n "yellowduck1" | md5sum | tr -d " -" >> hashes.txt
-echo -n "chickendrum" | md5 sum | tr -d " _" >> hashes.txt
+echo -n "Password" | sha512sum | tr -d " -" >> sha512.txt
+echo -n "HELLO" | sha512sum | tr -d " -" >> sha512.txt
+echo -n "MYSECRET" | sha512sum | tr -d " -" >> sha512.txt
+echo -n "Test1234" | sha512sum | tr -d " -" >> sha512.txt
+echo -n "P455w0rd" | sha512sum | tr -d " -" >> sha512.txt
+echo -n "GuessMe" | sha512sum | tr -d " -" >> sha512.txt
+echo -n "S3CuReP455Word" | sha512sum | tr -d " -" >> sha512.txt
+echo -n "yellowduck1" | sha512sum | tr -d " -" >> sha512.txt
+echo -n "chickendrum" | sha512sum | tr -d " _" >> sha512.txt
+echo -n "1231" | sha512sum | tr -d " _" >> sha512.txt
